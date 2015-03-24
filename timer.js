44c932a50749b9a1143890929da97e31c3c34eb2
Adam Gilgoff <agilgoff@gopro.com>
2015-03-23 19:42:52 -0700

    adds basic functioning timer.

--- timer.js
+++ timer.js
@@ -8,7 +8,34 @@ angular.module('timer', ['ui.router'])
     });
   }])
 
-  .controller('TimerCtrl', function() {
+  .controller('TimerCtrl', function($interval) {
     var timerCtrl = this;
+    timerCtrl.time = { seconds: null, minutes: null };
+    timerCtrl.displayTime = null;
+
+    timerCtrl.startTimer = function() {
+      parseTime();
+      var timerPromise = $interval(function(){
+        timerCtrl.parsedTime.subtract(1, 'seconds');
+        setDisplayTime();
+        if (timerCtrl.parsedTime.asMilliseconds() === 0) $interval.cancel(timerPromise);
+      }, 1000);
+    }
+
+    var parseTime = function() {
+      // ensure timerCtrl.time is formatted "07:07:07"
+      timerCtrl.parsedTime = moment.duration({
+        seconds: timerCtrl.time.seconds,
+        minutes: timerCtrl.time.minutes
+      });
+
+      setDisplayTime();
+    }
+
+    var setDisplayTime = function() {
+      timerCtrl.displayTime = timerCtrl.parsedTime.minutes() + ':' + timerCtrl.parsedTime.seconds();
+    }
+
+
   })
 ;
